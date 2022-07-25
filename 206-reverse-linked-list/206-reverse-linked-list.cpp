13fb1df8200576f4ab2ef10501d3811e765a4fc3
Raj Patidar <86468472+0Mecode@users.noreply.github.com>
2022-07-25 18:54:33 +0530

    Time: 18 ms (7.23%), Space: 8.5 MB (10.52%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.cpp
+++ 206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -1,28 +1,40 @@
 class Solution {
 public:
-    void fun(ListNode* &i, ListNode* j, int &N){
-        if(!j){
-            return;
+    // void fun(ListNode* &i, ListNode* j, int &N){
+    //     if(!j){
+    //         return;
+    //     }
+    //     fun(i, j->next, N);
+    //     if(N==0){
+    //         return;
+    //     }
+    //     swap(i->val, j->val);
+    //     N--;
+    //     i = i->next;
+    // }
+    
+    ListNode* fun(ListNode* j){
+        if(!j or !j->next){
+            return j;
         }
-        fun(i, j->next, N);
-        if(N==0){
-            return;
-        }
-        swap(i->val, j->val);
-        N--;
-        i = i->next;
+        ListNode* k = fun(j->next);
+        j->next->next = j;
+        j->next = NULL;
+        return k;
     }
     
     ListNode* reverseList(ListNode* head) {
-        int N = 0;
-        ListNode* ptr = head;
-        while(ptr){
-            ptr = ptr->next;
-            N++;
-        }
-        N = N/2;
-        ptr = head;
-        fun(ptr, head, N);
-        return head;
+        // int N = 0;
+        // ListNode* ptr = head;
+        // while(ptr){
+        //     ptr = ptr->next;
+        //     N++;
+        // }
+        // N = N/2;
+        // ptr = head;
+        // fun(ptr, head, N);
+        // return head;
+        
+        return fun(head);
     }
 };
