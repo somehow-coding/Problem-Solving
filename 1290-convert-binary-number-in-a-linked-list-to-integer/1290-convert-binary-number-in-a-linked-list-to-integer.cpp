6b783836a495205988ad74a677b15422bf85a934
Raj Patidar <86468472+0Mecode@users.noreply.github.com>
2022-07-25 18:03:03 +0530

    Time: 5 ms (27.07%), Space: 8.2 MB (62.46%) - LeetHub

--- 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
     int getDecimalValue(ListNode* head) {
-        int ans = 0, n = 0;
+        // int ans = 0, n = 0;
+        // ListNode* ptr = head;
+        // while(ptr){
+        //     ptr= ptr->next;
+        //     n++;
+        // }
+        // ptr = head, n--;
+        // while(ptr){
+        //     ans = ans + ((ptr->val)*pow(2, n));
+        //     n--;
+        //     ptr= ptr->next;
+        // }
+        // return ans;
+        int ans = 0;
         ListNode* ptr = head;
         while(ptr){
-            ptr= ptr->next;
-            n++;
-        }
-        ptr = head, n--;
-        while(ptr){
-            ans = ans + ((ptr->val)*pow(2, n));
-            n--;
-            ptr= ptr->next;
+            ans = ans * 2 + ptr->val;
+            ptr=ptr->next;
         }
         return ans;
     }
