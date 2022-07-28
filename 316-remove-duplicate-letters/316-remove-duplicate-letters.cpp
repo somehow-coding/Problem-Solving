08b86c10a2aec5337f9f6a49d2acb4c7db15789d
Raj Patidar <86468472+0Mecode@users.noreply.github.com>
2022-07-29 02:51:24 +0530

    Time: 3 ms (79.50%), Space: 6.9 MB (42.14%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -1,7 +1,27 @@
 class Solution {
 public:
-    //lagatar deletions karne ho to stack use karne ka sense banta hai...
-    //aabbcc
+    //logic:
+    /*
+    let say we go through the string s and we have the string str which is the 
+    desired string we need to find.
+    we need to decide either this character we have to take it or not to complete the str.
+    so how we decide?? based on conditions:
+    suppose we have not taken the character yet.
+    so let say we take it so suppose it is smaller character then previous characters
+    if we need lexicographical order we have to remove the previous characters such that
+    we should have them in the string ahead.
+    
+    if we have the character allready then it must be best lexicographical order uptill now.
+    we don't do anything.
+    */
+    //How stack is helping?? 
+    /*
+    suppose instead of stack we take a string str, which is storing characters.
+    so the logic remains the same but deleting smaller characters then previous characters
+    can be made easy using stack(o(1) time)
+    */
+    
+    //maps are used as per need.....
     string removeDuplicateLetters(string s) {
         map<char,int> totalFreq, curFreq;
         stack<char> st;
