6b1fbe34f122acec9b01670ad3d1353d37b5dde0
Raj Patidar <86468472+0Mecode@users.noreply.github.com>
2022-07-30 13:06:16 +0530

    Time: 1377 ms (6.07%), Space: 57.9 MB (80.22%) - LeetHub

---
+++ 916-word-subsets/916-word-subsets.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        vector<string> res;
+        map<char,int> mp, m, mp2;
+        for(string &s: words2){
+            for(char c: s){
+                m[c]++;
+            }
+            for(char c='a'; c<='z'; c++){
+                mp[c] = max(mp[c], m[c]);    
+                m[c] = 0;
+            }
+        }
+        for(string &s: words1){
+            int insert = 1;
+            for(char c: s){
+                mp2[c]++;
+            }
+            for(char c='a'; c<='z'; c++){
+                if(mp2[c]<mp[c]){
+                    insert = 0;
+                }
+                mp2[c] = 0;
+            }
+            if(insert){
+                res.push_back(s);
+            }
+        }
+        return res;
+    }
+};
