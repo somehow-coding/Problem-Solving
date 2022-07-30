31e3104ac6d0dd5e8d167116b67f573b9aff7262
Raj Patidar <86468472+0Mecode@users.noreply.github.com>
2022-07-30 13:06:58 +0530

    Time: 897 ms (14.61%), Space: 57.6 MB (80.45%) - LeetHub

--- 916-word-subsets/916-word-subsets.cpp
+++ 916-word-subsets/916-word-subsets.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
         vector<string> res;
-        map<char,int> mp, m, mp2;
+        unordered_map<char,int> mp, m, mp2;
         for(string &s: words2){
             for(char c: s){
                 m[c]++;
