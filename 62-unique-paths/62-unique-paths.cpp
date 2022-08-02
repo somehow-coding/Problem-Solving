85eb7a3893d91dc514b65db2edf1f56f032ed2cd
Raj Patidar <86468472+0Mecode@users.noreply.github.com>
2022-08-02 12:23:41 +0530

    Time: 5 ms (18.62%), Space: 5.9 MB (90.97%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -2,24 +2,24 @@ class Solution {
 public:
     int dp[101][101];
     
-    int fun(int i, int j, int m, int n){
-        if(i==m-1 and j==n-1){
+    int fun(int m, int n){
+        if(m==1 and n==1){
             return 1;
         }
-        if(i==m or j==n){
+        if(m==0 or n==0){
             return 0;
         }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
+        if(dp[m][n]!=-1){
+            return dp[m][n];
         }
         int ret = 0;
-        ret += fun(i+1, j, m, n);
-        ret += fun(i, j+1, m, n);
-        return dp[i][j] = ret;
+        ret += fun(m-1, n);
+        ret += fun(m, n-1);
+        return dp[m][n] = ret;
     }
     
     int uniquePaths(int m, int n) {
         memset(dp, -1, sizeof(dp));
-        return fun(0, 0, m, n);
+        return fun(m, n);
     }
 };
