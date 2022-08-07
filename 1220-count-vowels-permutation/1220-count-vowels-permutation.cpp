a30a64abdc2a40f8bc8a6b93c2cd84f76753da7f
Raj Patidar <86468472+0Mecode@users.noreply.github.com>
2022-08-07 13:13:44 +0530

    Time: 392 ms (11.84%), Space: 12.5 MB (59.66%) - LeetHub

---
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int N, mod = 1e9+7; 
+    
+    int dp[20005][6];
+    
+    unordered_map<char,set<char>> mp;
+    map<char,int> val;
+    int cntFun(int i, char p){
+        if(i==N){
+            return 1;
+        }
+        if(dp[i][val[p]]!=-1){
+            return dp[i][val[p]];
+        }
+        int cnt = 0;
+        for(char c:"aeiou"){
+            if(mp[p].count(c)){
+                cnt = (cnt + cntFun(i+1,c))%mod;
+            }
+        }
+        return dp[i][val[p]] = cnt;
+    }
+    
+    int countVowelPermutation(int n) {
+        mp['A'] = {'a','i','e','o','u'};
+        val['A'] = 0,
+        val['a'] = 1,
+        val['i'] = 2,
+        val['e'] = 3,
+        val['o'] = 4,
+        val['u'] = 5,
+        mp['a'] = {'e'}, mp['e'] = {'a','i'}, mp['i'] = {'a','e','o','u'};
+        mp['o'] = {'i', 'u'}, mp['u'] = {'a'};
+        N = n;
+        memset(dp,-1,sizeof(dp));
+        return cntFun(0, 'A');
+    }
+};
