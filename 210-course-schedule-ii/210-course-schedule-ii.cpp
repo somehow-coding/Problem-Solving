5b85fb26e0ecfb4ac9e6948a2e1f1dad9eb281b7
Raj Patidar <86468472+0Mecode@users.noreply.github.com>
2022-08-07 14:36:44 +0530

    Time: 39 ms (40.95%), Space: 14.3 MB (40.53%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -1,32 +1,64 @@
-class Graph{
-  public:
+class Graph {
+public:
     vector<vector<int>> graph;
     vector<int> visNodes;
     vector<int> vis;
     vector<int> arr;
-    Graph(vector<vector<int>> &edges, int N){
+    vector<int> indegree;
+    Graph(vector<vector<int>> &edges, int N) {
         graph.resize(N);
-        vis.resize(N,0);
-        visNodes.resize(N,0);
-        for(vector<int> edge: edges){
+        vis.resize(N, 0);
+        indegree.resize(N, 0);
+        visNodes.resize(N, 0);
+        for (vector<int> edge : edges) {
             graph[edge[0]].push_back(edge[1]);
+            indegree[edge[1]]++;
         }
     }
 };
 
+vector<int> topoLogicalSort(Graph &g, int N) {
+    queue<int> q;
+    for (int i = 0; i < N; i++) {
+        if (!g.indegree[i]) {
+            q.push(i);
+        }
+    }
+
+    while (q.size()) {
+        int t = q.size();
+        while (t--) {
+            int i = q.front();
+            q.pop();
+            g.arr.push_back(i);
+            for (int j : g.graph[i]) {
+                g.indegree[j]--;
+                if (!g.indegree[j]) {
+                    q.push(j);
+                }
+            }
+        }
+    }
+    reverse(begin(g.arr), end(g.arr));
+    if (g.arr.size() == N) {
+        return g.arr;
+    }
+    return {};
+}
+
 class Solution {
 public:
-    bool dfs(Graph &g, int i){
-        if(g.vis[i]){
+    bool dfs(Graph &g, int i) {
+        if (g.vis[i]) {
             return true;
         }
-        if(g.visNodes[i]){
+        if (g.visNodes[i]) {
             return false;
         }
         g.vis[i] = 1;
         g.visNodes[i] = 1;
-        for(int j: g.graph[i]){
-            if(dfs(g,j)){
+        for (int j : g.graph[i]) {
+            if (dfs(g, j)) {
                 return true;
             }
         }
@@ -34,16 +66,20 @@ public:
         g.vis[i] = 0;
         return false;
     }
-    
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+
+    vector<int> findOrder(int numCourses, vector<vector<int>> &prerequisites) {
         Graph g(prerequisites, numCourses);
-        for(int i=0; i<numCourses; i++){
-            if(!g.visNodes[i]){
-                if(dfs(g,i)){
-                    return {};
-                }
-            }
-        }
-        return g.arr;
+
+        // dfs
+        //  for(int i=0; i<numCourses; i++){
+        //      if(!g.visNodes[i]){
+        //          if(dfs(g,i)){
+        //              return {};
+        //          }
+        //      }
+        //  }
+
+        // bfs
+        return topoLogicalSort(g, numCourses);
     }
 };
