8d61327ef14f7c3452a9c40ffb8e8f26db248356
Raj Patidar <86468472+0Mecode@users.noreply.github.com>
2022-08-09 00:59:30 +0530

    Time: 184 ms (42.26%), Space: 67.7 MB (54.07%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,5 +1,14 @@
 class Solution {
 public:
+    /*
+    we maintain a max_sum and cur_sum.
+    we add this value of element to cur_sum and update the max_sum.
+    
+    it makes no sense to keep elements which sums up to a negative value,
+    so we slide(update cur_sum to zero)
+    
+    */
+    
     int maxSubArray(vector<int>& nums) {
         int max_sum = INT_MIN, cur_sum = 0;
         for(int num: nums){
