97050f2f284a6cc5eb823c4bab406ded43bfa477
Raj Patidar <86468472+0Mecode@users.noreply.github.com>
2022-08-09 00:33:54 +0530

    Time: 19 ms (5.84%), Space: 12 MB (93.11%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,6 +1,50 @@
 class Solution {
 public:
+    
+    /*
+    function returning index of element having value just greater than val
+    used binary search algorithm.
+    */
+    int bs(vector<int> &nums, int start, int end, int val){
+        int reti = -1;
+        while(start<=end){
+            int mid = (start + end) >> 1;
+            if(nums[mid]>val){
+                reti = mid;
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+        return reti;
+    }
+    
+    /*
+    function to reverse elements of the array from index start to index start
+    */
+    
+    void rev(vector<int> &nums, int start, int end){
+        while(start<=end){
+            swap(nums[start++],nums[end--]);
+        }
+    }
+    
     void nextPermutation(vector<int>& nums) {
-        next_permutation(begin(nums),end(nums));
+        int j = size(nums) - 1, i = size(nums) - 2, permutated = 0;
+        
+        while(i>=0){
+            if(nums[i]<nums[i+1]){
+                permutated = 1;
+                int k = bs(nums, i+1, j, nums[i]);
+                swap(nums[k],nums[i]);
+                rev(nums, i+1, j);
+                break;
+            }
+            i--;
+        }
+        if(!permutated){
+            rev(nums, 0, j);
+        }
     }
 };
