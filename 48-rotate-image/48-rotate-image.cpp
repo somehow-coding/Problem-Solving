64d8533df110328fa92b1df800e3a912f009ffe5
Raj Patidar <86468472+0Mecode@users.noreply.github.com>
2022-08-10 00:04:56 +0530

    Time: 4 ms (43.11%), Space: 7.2 MB (33.91%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -18,7 +18,26 @@ public:
         fun(matrix, i+1,j+1,I-1,J-1);
     }
     
+    //below is unintuitive solution....
+    //basic maths(may be puzzle thing)
+    
+    //if your reverse matrix that is last row become first row and so on.
+    //then take the transpose of the matrix.
+    //this two steps will result the final matrix...    
+    
+    void transpose(vector<vector<int>>& matrix){
+        for(int i=0;i<size(matrix);i++){
+            for(int j=i+1;j<size(matrix);j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+    }
+    
     void rotate(vector<vector<int>>& matrix) {
-        fun(matrix,0,0,size(matrix)-1,size(matrix)-1);
+        // fun(matrix,0,0,size(matrix)-1,size(matrix)-1);
+        
+        //second method.
+        reverse(begin(matrix),end(matrix));
+        transpose(matrix);
     }
 };
