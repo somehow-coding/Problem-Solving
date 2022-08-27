5aca6876227eaaf0772498d92c7ae5c81f8e092a
Raj Patidar <86468472+0Mecode@users.noreply.github.com>
2022-08-27 22:15:18 +0530

    Time: 20 ms (74.68%), Space: 19.8 MB (82.05%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     /*
-    pani at cur building  = min(leftmaxheight, rightmaxheight)\
+    water at cur building  = min(leftmaxheight, rightmaxheight)
     do it for all the buildings.
     */
     
     int trap(vector<int>& height) {
+        /*
         int total = 0, n = size(height);
         int right[n];
         right[n-1] = height[n-1];
@@ -20,5 +21,42 @@ public:
             left = max(left, height[i]);
         }
         return total;
+        */
+        //above approch will need extra O(n) space 
+        
+        // more optimal approach used 2 pointers
+        
+        // in this approach maintains two values left_max and right_max
+        // and traverse the array using two pointers.
+        int left_max = 0, right_max = 0, left = 0, right = height.size()-1;
+        int water_trapped = 0;
+        while(left<=right){
+            if(height[left]<=height[right]){
+                if(left_max<height[left]){
+                    left_max = height[left];
+                }
+                else{
+                    /**how part???*/
+                    //how are we sure that there exists a height which is 
+                    //greater than or equal to left_max is that if it wasn't 
+                    //there we might not have reached here at the cur height.
+                    //so there must be a greate height on right than only we 
+                    // are able to move from left most height to uptil now(left)
+                    water_trapped += (left_max-height[left]);
+                }
+                left++;
+            }
+            else{
+                if(right_max<height[right]){
+                    right_max = height[right];
+                }
+                else{
+                    /**how part??? is cleared above*/
+                    water_trapped += (right_max-height[right]);
+                }
+                right--;
+            }
+        }
+        return water_trapped;
     }
 };
