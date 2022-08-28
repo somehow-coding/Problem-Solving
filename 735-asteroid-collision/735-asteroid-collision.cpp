3668a07aed9681b78e3494ae0c29bc8f1fbc8bf0
Raj Patidar <86468472+0Mecode@users.noreply.github.com>
2022-08-28 14:29:06 +0530

    Time: 19 ms (69.19%), Space: 17.8 MB (57.99%) - LeetHub

--- 735-asteroid-collision/735-asteroid-collision.cpp
+++ 735-asteroid-collision/735-asteroid-collision.cpp
@@ -14,10 +14,15 @@ public:
     //questions to be asked??
     //is zero allowed very important to know this to write solution...
     
-    bool opposite(int a, int b){
+    //to be observed(sense..)->they will colide if only if + -> and - <-
+    //that means positive on left and negative on right then only than they will collide.
+    
+    // returns if two asteroids can collide.
+    bool canCollide(int a, int b){
         return (a>0 and b<0);
     }
     
+    // returns out of 2 asteroids which will left after collision.
     int asteroidLeft(int a, int b){
         if(abs(a)==abs(b)){
             return 0;
@@ -27,35 +32,41 @@ public:
         }
         return b;
     }
+    
+    // returns final state of asteroids after collision.
     vector<int> asteroidCollision(vector<int>& asteroids) {
+        // stack for tracking dynamic state of asteroids.
         stack<int> st;
         for(int asteroid: asteroids){
-            bool flag = 0;
-            while(st.size() and opposite(st.top(),asteroid)){
+            // flag is maintained for checking whether to push current asteroid to stack.
+            bool flag = 1;
+            while(st.size() and canCollide(st.top(),asteroid)){
                 int left = asteroidLeft(st.top(),asteroid);
                 if(left==0){
                     st.pop();
-                    flag = 1;
+                    // if both got exploded then the current asteroid should not be pushed.
+                    flag = 0;
                     break;
                 }
                 else if(left==asteroid){
                     st.pop();
                 }
                 else{
-                    flag = 1;
+                    // if the current got exploded than can't be pushed.
+                    flag = 0;
                     break;
                 }
             }
             if(flag == 1){
-                continue;
-            } 
-            st.push(asteroid);
+                st.push(asteroid);
+            }     
         }
-        vector<int> final_state(st.size());
+        //final state of asteroids.
+        vector<int> state(st.size());
         while(st.size()){
-            final_state[st.size()-1] = st.top();
+            state[st.size()-1] = st.top();
             st.pop();
         }
-        return final_state;
+        return state;
     }
 };
