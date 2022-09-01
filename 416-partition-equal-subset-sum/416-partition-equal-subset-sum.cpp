857311a8ccc74214f7613a53e55dbc94349d6c45
Raj Patidar <86468472+0Mecode@users.noreply.github.com>
2022-09-01 11:26:19 +0530

    Time: 385 ms (44.26%), Space: 27.4 MB (39.58%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -20,8 +20,32 @@ public:
         int sum = accumulate(begin(nums),end(nums),0);
         
         if(sum & 1) return false;
+        // memset(dp, -1, sizeof(dp));
+        // return fun(nums, nums.size(), sum/2);
+        sum /= 2;
         
-        memset(dp, -1, sizeof(dp));
-        return fun(nums, nums.size(), sum/2);
+        int n = nums.size(), t[n+1][sum+1];
+        
+        for(int j = 1; j <= sum; j++) {
+            t[0][j] = 0;
+        }
+        
+        for(int i = 0; i <= n; i++) {
+            t[i][0] = 1;
+        }
+        
+        
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= sum; j++) {
+                if(j - nums[i-1] >= 0) {
+                    t[i][j] = t[i-1][j-nums[i-1]] | t[i-1][j];
+                }
+                else {
+                    t[i][j] = t[i-1][j];
+                }
+            }
+        }
+        
+        return t[n][sum];
     }
 };
